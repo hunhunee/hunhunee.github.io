6d2ba095df0d5ce4153620a31026f2067e099dc2
hunhunee <806125872@qq.com>
2016-07-12 16:42:50 +0800

    aaa

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,12 @@ if ($(window).width() <= 1280) {
 }
 
 // Variables
-    tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag1       = $('.xiaossss'),
+    tag2       = $('.home'),
+    tag3       = $('.word'),
+    tag4       = $('.life'),
+    tag5       = $('.test'),
+    tag6       = $('.lin');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
